db5bbdc469283b491a83a6fc8e228abead98770a
Alexandra789 <zakovinko99@gmail.com>
2025-06-25 15:35:04 +0300

    change 'class' to 'classname'

--- ishop0/components/Shop.js
+++ ishop0/components/Shop.js
@@ -6,7 +6,7 @@ class Shop extends React.Component {
   render() {
     const { name, address } = this.props;
     return (
-      <div class="shop-sign">
+      <div className="shop-sign">
         <h1 className="shop-sign__title">{name}</h1>
         <p className="shop-sign__adress">{address}</p>
       </div>
