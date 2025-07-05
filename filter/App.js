6afd5e725a7b0f39a6b2bf978ddc5ac97bf3f313
Alexandra789 <zakovinko99@gmail.com>
2025-07-05 22:18:12 +0300

    init project

---
+++ filter/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Filter from './components/Filter';
+
+import './App.css';
+const STRINGS_LIST = ['california', 'everything', 'aboveboard', 'washington', 'basketball', 'weathering', 'characters', 'literature', 'contraband', 'appreciate'];
+
+ReactDOM.render(
+  <Filter strings={STRINGS_LIST}/>,
+  document.getElementById('container')
+);
---
+++ filter/components/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Filter.css';
+
+
+class Filter extends React.Component {
+    render() {
+        const { strings } = this.props;
+    }
+}
+
+export default Filter;
