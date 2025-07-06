904271689e0ba26abf0e980584c1ede20705b714
Alexandra789 <zakovinko99@gmail.com>
2025-07-06 16:06:58 +0300

    implement Filter

--- filter/components/Filter.js
+++ filter/components/Filter.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Filter.css';
 
-
 class Filter extends React.Component {
+    static propTypes = {
+        strings: PropTypes.arrayOf(PropTypes.string),
+    };
+
+    state = {
+        filterValue: '',
+        isChecked: false,
+    }
+
+    changeFilterValue = e => {
+        this.setState({ filterValue: e.target.value });
+    };
+
+    toggleIsChecked = () => {
+        this.setState({ isChecked: !this.state.isChecked });
+    }
+
+    resetFilter = () => {
+        this.setState({ filterValue: '', isChecked: false });
+    }
+
     render() {
         const { strings } = this.props;
+
+        const filteredList = strings.filter((item) => item.includes(this.state.filterValue));
+
+        this.state.isChecked ? filteredList.sort() : filteredList;
+
+        return (
+            <div className="filter">
+                <div className="filter__panel">
+                    <input type="checkbox" checked={this.state.isChecked} onChange={this.toggleIsChecked}></input>
+                    <input type="text" className="filter__input" value={this.state.filterValue} onChange={this.changeFilterValue} autoFocus></input>
+                    <button onClick={this.resetFilter}>сброс</button>
+                </div>
+                <div className='filter__result'>
+                    {
+                        filteredList.map((item, index) => <p key={index}>{item}</p> )
+                    }
+                </div>
+            </div>
+        );
     }
 }
 
