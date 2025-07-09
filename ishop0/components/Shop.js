800382e7424588d443dfbfbc1a817f38ce11ac8a
Alexandra789 <zakovinko99@gmail.com>
2025-07-09 21:17:34 +0300

    add propTypes

--- ishop0/components/Shop.js
+++ ishop0/components/Shop.js
@@ -1,9 +1,22 @@
 ﻿import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Shop.css';
 import Product from './Product';
 
 class Shop extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    products: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      quantity: PropTypes.number.isRequired,
+    })),
+  };
+
   render() {
     const { title, address, products } = this.props;
     return (
