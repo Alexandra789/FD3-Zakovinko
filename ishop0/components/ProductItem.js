dd07674788c464edfc8c82ba337674d27272ee87
Alexandra789 <zakovinko99@gmail.com>
2025-07-09 21:18:12 +0300

    implement productItem

--- ishop0/components/ProductItem.js
+++ ishop0/components/ProductItem.js
@@ -1,23 +1,33 @@
 import React from 'react';
-
-import './ProductItem.css';
+import PropTypes from 'prop-types';
 
 class ProductItem extends React.Component {
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        quantity: PropTypes.number.isRequired,
+        cbSelected: PropTypes.func.isRequired,
+        isSelected: PropTypes.bool.isRequired
+
+    };
+
+    rowClicked = e => {
+        this.props.cbSelected(e.currentTarget.getAttribute('data-row-index'));
+    };
+
     render() {
-        const { title, price, url, stock } = this.props;
+        const { id, name, price, url, quantity, isSelected } = this.props;
+
         return (
-            <li className="product__item">
-                <div className="product__card">
-                    <img className="product__image" src={url} width={400} height={400} alt={title} />
-                    <div className="product__info">
-                        <div className="product__meta">
-                            <data className="product__price" value={price}>{price} р.</data>
-                            <data className="product__stock" value={stock}>{stock} шт.</data>
-                        </div>
-                        <h3 className="product__title">{title}</h3>
-                    </div>
-                </div>
-            </li>
+            <tr className={isSelected && "active"} data-row-index={id} onClick={this.rowClicked}>
+                <th>{name}</th>
+                <th>{price}</th>
+                <th>{url}</th>
+                <th>{quantity}</th>
+                <th><button>Delete</button></th>
+            </tr>
         );
     }
 }
