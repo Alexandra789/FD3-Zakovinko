d0b4c77c249cdadf8c027a5d351ea05c02817a3f
Alexandra789 <zakovinko99@gmail.com>
2025-07-09 21:18:55 +0300

    add propTypes and implement selectRow function

--- ishop0/components/Product.js
+++ ishop0/components/Product.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Product.css';
 import ProductItem from './ProductItem.js';
 
-
-
 class Product extends React.Component {
+    static propTypes = {
+        products: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            price: PropTypes.string.isRequired,
+            url: PropTypes.string.isRequired,
+            quantity: PropTypes.number.isRequired,
+        })),
+    };
+
+    state = {
+        selectedRowIndex: null,
+    }
+
+    onSelectRow = (rowIndex) => {
+        this.setState({ selectedRowIndex: rowIndex });
+    }
+
     render() {
         const { products } = this.props;
 
         const productItem = products.map(item =>
-            <ProductItem key={item.id} title={item.title} price={item.price} url={item.url} stock={item.stock} />
+            <ProductItem
+                key={item.id} id={item.id} name={item.name}
+                price={item.price} url={item.url} quantity={item.quantity}
+                cbSelected={this.onSelectRow} isSelected={this.state.selectedRowIndex == item.id} />
         );
 
         return (
-            <ul className="product">
-                {productItem}
-            </ul>
+            <table className='product-table'>
+
+                <tbody>
+                    <tr>
+                        {Object.keys(products[0]).map((item, index) =>
+                            index === 0 || <th key={index}>{item}</th>
+                        )}
+                        <th>control</th>
+                    </tr>
+                    {productItem}
+                </tbody>
+            </table>
         );
     }
 }
