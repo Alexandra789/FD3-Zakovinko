b1b2316601d61a376ca582583289adf7e1cff99d
Alexandra789 <zakovinko99@gmail.com>
2025-07-23 12:52:19 +0300

    formatting file

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -10,8 +10,8 @@ class RainbowFrame extends React.Component {
         return (
             <div className="RainbowFrame">
                 {colors.reduce((accamulator, currentValue, index) => {
-                    return <div key={index} style={{border:`5px solid ${currentValue}`, color:currentValue, padding: '10px', margin: '10px'}}>{accamulator}</div>
-                }, <div style={{textAlign:'center'}}>{children}</div>)}
+                    return <div key={index} style={{ border: `5px solid ${currentValue}`, color: currentValue, padding: '10px', margin: '10px' }}>{accamulator}</div>
+                }, <div style={{ textAlign: 'center' }}>{children}</div>)}
             </div>
         );
     }
