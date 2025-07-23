a7945dd6d80152c682378d73b152ebef3490e846
Alexandra789 <zakovinko99@gmail.com>
2025-07-23 10:29:51 +0300

    init project

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+    render() {
+        return (
+            <div className="RainbowFrame">
+                Hello!
+            </div>
+        );
+    }
+}
+
+export default RainbowFrame;
