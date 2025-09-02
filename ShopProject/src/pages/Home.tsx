4cb20e6a51efc5d894dd1bfbccddafd61f4fd9c0
Alexandra789 <zakovinko99@gmail.com>
2025-09-02 20:54:28 +0300

    add cartItemsCount

--- ShopProject/src/pages/Home.tsx
+++ ShopProject/src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import { Products } from '../components/Products/Products';
 
 export const Home = () => {
+    const [cartItemsCount, setCartItemsCount] = useState<number>(0);
+    
     return (
-        <Products/>
+        <Products cartItemsCount={cartItemsCount} setCartItemsCount={setCartItemsCount}/>
     )
 }
