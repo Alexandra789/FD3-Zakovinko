a1e29f30cf08ad823f66d88984cfecf73f3672a8
Alexandra789 <zakovinko99@gmail.com>
2025-09-02 20:54:03 +0300

    rename type props

--- ShopProject/src/pages/Cart.tsx
+++ ShopProject/src/pages/Cart.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
-type CartProps = {
+type CartPropTypes = {
     count: number,
 }
 
-export const Cart = ({ count }: CartProps) => {
+export const Cart = ({ count }: CartPropTypes) => {
     return (
         <h1>CART {count}</h1>
     )
