d91a3d3918d35e6c1c27917aace206df2eebc9d8
Alexandra789 <zakovinko99@gmail.com>
2025-09-03 13:14:13 +0300

    components Products in progress

--- ShopProject/src/components/Products/Products.tsx
+++ ShopProject/src/components/Products/Products.tsx
@@ -1,42 +1,77 @@
 import useSWR from 'swr';
-import { type ProductType } from '../../types/index'
-
 import './Products.css';
+import { Link } from 'react-router-dom';
 
 const fetcher = (url: string): Promise<ProductType[]> => fetch(url).then((res) => res.json());
 
-export const Products = () => {
+type ProductsPropTypes = {
+    cartItemsCount: number;
+    setCartItemsCount: (newCartItemsCount: number) => void;
+};
+
+type ProductType = {
+    id: number,
+    images: string[],
+    title: string,
+    description: string,
+    price: number
+};
+
+export const Products = ({ cartItemsCount, setCartItemsCount }: ProductsPropTypes) => {
     const ALL_PRODUCTS_URL = 'https://api.escuelajs.co/api/v1/products';
     const { data, error, isLoading } = useSWR<ProductType[]>(
         ALL_PRODUCTS_URL,
         fetcher
     );
-    if (error) return <div>An error has occurred.</div>;
-    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div className="container">An error has occurred.</div>;
+    if (isLoading) return <div className="container">Loading...</div>;
 
-    if (!data || !Array.isArray(data)) return <div>No products found</div>;
+    if (!data || !Array.isArray(data)) return <div className="container">No products found</div>;
+
+    const incrementCount = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        setCartItemsCount(++cartItemsCount);
+    }
+    const decrementCount = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        setCartItemsCount(--cartItemsCount);
+    }
 
     return (
         <div className="Products">
-            <h2 className="products-list-title">Products List</h2>
-            <div className="products-list">
-                {data.map((product: ProductType) => (
-                    <div key={product.id} className="product-card">
-                        <img className="product-image"
-                            src={product.images[0]}
-                            alt={product.title}
-                        />
-                        <div className="product-info">
-                            <h3 className="product-title">{product.title}</h3>
-                            <p className="product-description">{product.description}</p>
+            <div className="container">
+                <h2 className="products-list-title">Products List</h2>
+                <div className="products-list">
+                    {data.map((product: ProductType) => (
+                        <div key={product.id} className="product-card">
+                            <Link to={`/product/${product.id}`}>
+                                <img className="product-image"
+                                    src={product.images[0]}
+                                    alt={product.title}
+                                />
+                                <div className="product-info">
+                                    <h3 className="product-title">{product.title}</h3>
+                                    <p className="product-description">{product.description}</p></div></Link>
                             <div className="cart-info">
                                 <p><strong>Price: ${product.price}</strong></p>
-                                <button className="btn btn-secondary"> <i className="bi bi-cart3"></i></button>
+                                {
+                                    cartItemsCount === 0 &&
+                                    <button className="btn btn-secondary" onClick={e => incrementCount(e)}>
+                                        <i className="bi bi-cart3"></i>
+                                    </button>
+                                }
+                                {
+                                    cartItemsCount !== 0 &&
+                                    <div className="count">
+                                        <button className="btn btn-secondary" onClick={e => decrementCount(e)}>-</button>
+                                        <p>{cartItemsCount}</p>
+                                        <button className="btn btn-secondary" onClick={e => incrementCount(e)}>+</button>
+                                    </div>
+                                }
                             </div>
                         </div>
-
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
